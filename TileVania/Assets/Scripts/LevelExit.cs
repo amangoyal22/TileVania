1e0c2fb268455075a97dbaab148df62e6ca2f561
amangoyal <aman.goyalwd@gmail.com>
2020-09-12 22:54:16 +0530

    next lvl loader

---
+++ TileVania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{    
+    //config
+    [SerializeField] private float LevelLoadDelay = 2f;
+    [SerializeField] private float LevelExitSlowFactor = 0.2f;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());   
+    }
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = LevelExitSlowFactor;
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        Time.timeScale = 1f;
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
