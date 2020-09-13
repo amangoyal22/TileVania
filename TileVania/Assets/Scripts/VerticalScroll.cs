18cbe2b675e1a95c3ab0f441b8f31384c8d8ee70
amangoyal <aman.goyalwd@gmail.com>
2020-09-13 11:39:06 +0530

    water hazards

---
+++ TileVania/Assets/Scripts/VerticalScroll.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalScroll : MonoBehaviour
+{   
+    [Tooltip("Game Units per Second")]
+    [SerializeField] private float scrollRate = 0.2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yMove = scrollRate * Time.deltaTime;
+     transform.Translate(new Vector2(0f,yMove));   
+    }
+}
