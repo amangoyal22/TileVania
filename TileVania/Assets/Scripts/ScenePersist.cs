5120b10311c1c27e28f4b17a2015e8bbf859860d
amangoyal <aman.goyalwd@gmail.com>
2020-09-13 11:12:14 +0530

    coin remeber

---
+++ TileVania/Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePersist : MonoBehaviour
+{
+    private int startingSceneIndex;
+    private void Awake()
+    {
+        int numGameSessions = FindObjectsOfType<ScenePersist>().Length;
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        startingSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex != startingSceneIndex)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
